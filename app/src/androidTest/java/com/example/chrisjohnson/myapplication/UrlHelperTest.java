e96b0dd36d73be00b75e9aedb31b49b89094fc11
Chris Johnson <chrisjohnson757@gmail.com>
2014-12-12 16:07:27 +0000

    Fixing typos in example URLs in UrlHelper test case Added test for HTTPS URLs

--- app/src/androidTest/java/com/example/chrisjohnson/myapplication/UrlHelperTest.java
+++ app/src/androidTest/java/com/example/chrisjohnson/myapplication/UrlHelperTest.java
@@ -10,19 +10,26 @@ import junit.framework.Assert;
  */
 public class UrlHelperTest extends AndroidTestCase {
 
-    public static final String URL_CORRECT_FORMAT = "http://example/com";
-    public static final String URL_MISSING_PROTOCOL = "example/com";
+    public static final String URL_CORRECT_FORMAT = "http://example.com";
+    public static final String URL_CORRECT_FORMAT_HTTPS = "https://example.com";
+    public static final String URL_MISSING_PROTOCOL = "example.com";
 
     @SmallTest
-    public void testCheckNormaliseUrlAddsProtocolTest() {
+    public void testCheckNormaliseUrlAddsProtocol() {
         UrlHelper helper = new UrlHelper();
         Assert.assertEquals(URL_CORRECT_FORMAT,helper.NormaliseUrl(URL_MISSING_PROTOCOL));
     }
 
     @SmallTest
-    public void testCheckNormaliseUrlIgnoresCorrectlyFormattedUrlTest() {
+    public void testCheckNormaliseUrlIgnoresCorrectlyFormattedUrl() {
         UrlHelper helper = new UrlHelper();
         Assert.assertEquals(URL_CORRECT_FORMAT,helper.NormaliseUrl(URL_CORRECT_FORMAT));
+    }
+
+    @SmallTest
+    public void testCheckNormaliseUrlIgnoresCorrectlyFormattedHttpsUrl() {
+        UrlHelper helper = new UrlHelper();
+        Assert.assertEquals(URL_CORRECT_FORMAT_HTTPS,helper.NormaliseUrl(URL_CORRECT_FORMAT_HTTPS));
     }
 
 }
